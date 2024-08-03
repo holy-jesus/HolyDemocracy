7c92ff8afedd97b22326c050ae43c0c300c956a2
holy-jesus <hj@holy-coder.ru>
2024-08-04 04:18:58 +0500

    Продолжаю делать настройки

--- commands/settings.js
+++ commands/settings.js
@@ -10,6 +10,7 @@ import { isAdministrator, sendMessage } from "../utils.js";
 function getHelp(settings) {
   let spaces = Math.max(...Object.keys(settings).map((value) => value.length));
   let text = "Ключ" + " ".repeat(spaces - 4) + " | " + "Текущее значение\n";
+  text += "_".repeat(text.split("|")[0].length) + "|" + "_".repeat(text.split("|")[1].length) + '\n'
   for (let [key, value] of Object.entries(settings)) {
     text += key + " ".repeat(spaces - key.length) + " | " + value + "\n";
   }
@@ -23,28 +24,47 @@ function getHelp(settings) {
   );
 }
 
+/**
+ *
+ * @param {String} key
+ * @param {Chat.settings} settings
+ * @returns {String}
+ */
+function getHelpSetting(key, settings) {
+  return settings[key].toString();
+}
+
 bot.onText("/settings", async (msg) => {
+  let chatObj = null;
   if (msg.chat.type == "private") {
     const chats = await Chat.find({
       $or: [{ admins: msg.from.id }, { creator: msg.from.id }],
     });
-    console.log(chats, msg.from.id);
 
-    if (chats.length == 0) {
-      await sendMessage(
-        msg.chat.id,
-        "У вас нету чатов, в которых вы являетесь администратором."
-      );
-      return;
-    } else {
-      await sendMessage(msg.chat.id, getHelp(chats[0].settings));
-      return;
+    if (chats.length == 1) {
+      chatObj = chats[0];
+    } else if (chats.length > 1) {
     }
   } else {
     if (!(await isAdministrator(msg.chat.id, msg.from.id))) return;
 
-    const chatObj = await Chat.findById(msg.chat.id);
+    chatObj = await Chat.findById(msg.chat.id);
+  }
 
+  if (!chatObj) {
+    await sendMessage(
+      msg.chat.id,
+      "У вас нету чатов, в которых вы являетесь администратором."
+    );
+    return;
+  }
+
+  const args = msg.text.split(" ").filter((value) => value != "");
+
+  if (args.length == 1 || args.length > 3) {
     await sendMessage(msg.chat.id, getHelp(chatObj.settings));
+  } else if (args.length == 2) {
+    await sendMessage(msg.chat.id, getHelpSetting(args[1], chatObj.settings))
+  } else if (args.length == 3) {
   }
 });
